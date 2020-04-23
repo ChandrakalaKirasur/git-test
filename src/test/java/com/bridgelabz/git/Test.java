748edb55cf7791f64383a8cd198d476e4dc12bca
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-04-24 03:11:37 +0530

    [ChandrakalaKirasur]pull and merge completed

--- src/test/java/com/bridgelabz/git/Test.java
+++ src/test/java/com/bridgelabz/git/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	public void checkMethod() {
 		System.out.println("testing git commit");
 		System.out.println("if by mistake I commit in master and I wan tthat in other branch then I can use checkpick with hash number");
+		System.out.println("done with merge ");
 	}
 }
