836583dc1a5d7352213fc2fb2673afaa8e5b1e40
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-04-24 00:40:02 +0530

    [ChandrakalaKirasur]initial commit

---
+++ src/main/java/com/bridgelabz/git/GitTestApplication.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.git;
+
+import org.springframework.boot.SpringApplication;
+
+public class GitTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestApplication.class, args);
+	}
+}
