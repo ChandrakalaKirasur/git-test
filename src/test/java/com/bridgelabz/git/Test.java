5b1e7270d1b0a23d54683635460c79d17e35cddb
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-04-24 02:57:55 +0530

    [ChandrakalaKirasur]checking cherry pick

--- src/test/java/com/bridgelabz/git/Test.java
+++ src/test/java/com/bridgelabz/git/Test.java
@@ -4,5 +4,6 @@ public class Test {
 
 	public void checkMethod() {
 		System.out.println("testing git commit");
+		System.out.println("if by mistake I commit in master and I wan tthat in other branch then I can use checkpick with hash number");
 	}
 }
